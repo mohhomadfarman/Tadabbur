24943753a7b5f2c55c9e4d72571a76aa6d73d584
Muhammad Farman <60259603+mohhomadfarman@users.noreply.github.com>
2026-06-20 02:47:41 +0530

    Update

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -923,7 +923,7 @@
             <svg style="color:#f87171;" class="w-3.5 h-3.5 inline shrink-0" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"/></svg>
             for the Ummah.
           </p>
-          <p class="arabic-text text-sm" style="color:rgba(255,255,255,0.2); line-height:1.4;">بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</p>
+          <p class="arabic-text text-sm" style="color:rgba(255,255,255,0.2); line-height:1.4;">Developed By <a href="http://shapesky.net" target="_blank" rel="noopener ">shapesky.net</a></p>
         </div>
 
       </div>
